05bacfd07cb1bea86f5e98de03835c1b8366e236
tapframe <85391825+tapframe@users.noreply.github.com>
2026-09-22 15:16:41 +0530

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,8 +105,8 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1062
-        versionName = "1.0.0"
+        versionCode = 1064
+        versionName = "1.1.0-beta.1"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
