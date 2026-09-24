dae7c3c26b13ee689874484f302ebb4f7ff31991
ceschini86 <252812870+ceschini86@users.noreply.github.com>
2026-09-24 12:46:02 -0300

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,8 +105,8 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1072
-        versionName = "1.0.8"
+        versionCode = 1073
+        versionName = "1.0.9"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
